8ae548179292960c09aa2ee75da39f5bce5fb406
antonbriganti <anton.briganti@gmail.com>
2017-08-29 13:16:06 +1000

    updated aws to reflect new split design of character and creator

--- dnd_character_creator/character_generator/aws_driver.py
+++ dnd_character_creator/character_generator/aws_driver.py
@@ -1,7 +1,8 @@
-from character_generator import Character
+#!/usr/local/bin/python3.6
+from character_generator import Character, CharacterCreator
 import pprint, json
 
 def lambda_handler(json_input, context):
-    char = Character(True, True, True, False)
-    char.char_generator()
-    return (char.generate_json())
+    creator = CharacterCreator(True, True, True, False)
+    char = creator.char_generator()
+    return(char.generate_json())
